b96e5f9e9693012b401396e4de1187f271e7f139
Sarah Daniel <sarahdanbolt@gmail.com>
2023-01-06 00:58:55 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import styles from './style'
+import { Navbar, Hero,  Footer,  Billing, Business, Button, CardDeal, Clients, CTA, Stats, Testimonial } from './components';
+
+const App = () => {
+  return (
+    <div className="bg-primary w-full overflow-hidden">
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar />
+        </div>
+        </div>
+
+        <div className={`bg-primary ${styles.flexStart}`}>
+          <div className={`${styles.boxWidth}`}>
+            <Hero />
+          </div>
+        </div>
+
+        <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+          <div className={`${styles.boxWidth}`}>
+            <Stats />
+            <Business />
+            <Billing />
+            <CardDeal />
+            <Testimonial />
+            <Clients />
+            <CTA /> 
+            <Footer/>
+          </div>
+        </div>
+
+      </div>
+  );
+}
+
+export default App;
---
+++ src/components/index.js
@@ -0,0 +1,13 @@
+export { default as Billing } from './Billing';
+export { default as Business } from './Business';
+export { default as Button } from './Button';
+export { default as CardDeal } from './CardDeal';
+export { default as Clients } from './Clients';
+export { default as CTA } from './CTA';
+export { default as FeedbackCard } from './FeedbackCard';
+export { default as Footer } from './Footer';
+export { default as GetStarted } from './GetStarted';
+export { default as Hero } from './Hero';
+export { default as Navbar } from './Navbar';
+export { default as Stats } from './Stats';
+export { default as Testimonial } from './Testimonial';
